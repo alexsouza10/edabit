356d633f306051b4a824516f75a70d54d8b89c3f
Alex.Araújo <125103074+alexsouza10@users.noreply.github.com>
2023-09-02 12:29:49 -0300

    first commit

---
+++ Python/Medium/lengthOfNumber.py
@@ -0,0 +1,12 @@
+def number_length(num):
+    return len(str(num))
+
+
+length1 = number_length(10)
+print(length1)  # Print 2
+
+length2 = number_length(5000)
+print(length2)  # Print 5
+
+length3 = number_length(0)
+print(length3)  # Print 1
