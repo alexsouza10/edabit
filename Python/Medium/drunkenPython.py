b007c448285e3f0b65d6eb6bbf2e46ea05528247
alexsouza10 <alexsouza160298@gmail.com>
2023-09-02 12:31:09 -0300

    first commit

---
+++ Python/Medium/drunkenPython.py
@@ -0,0 +1,15 @@
+def int_to_str(num):
+    return str(num)
+
+def str_to_int(string):
+    return int(string)
+
+# Examples:
+result1 = int_to_str(4)
+print(result1)  # Should print "4"
+
+result2 = str_to_int("4")
+print(result2)  # Should print 4
+
+result3 = int_to_str(29348)
+print(result3)  # Should print "29348"
