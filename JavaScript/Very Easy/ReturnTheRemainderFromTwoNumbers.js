d4508f200c7a27e1f31cf8cd479e0444085b0f9b
alexsouza10 <alexsouza160298@gmail.com>
2023-11-16 06:51:04 -0300

    feat: statement

--- JavaScript/Very Easy/ReturnTheRemainderFromTwoNumbers.js
+++ JavaScript/Very Easy/ReturnTheRemainderFromTwoNumbers.js
@@ -1,3 +1,17 @@
+/*
+Return the Remainder from Two Numbers
+There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+Examples
+remainder(1, 3) ➞ 1
+
+remainder(3, 4) ➞ 3
+
+remainder(-9, 45) ➞ -9
+
+remainder(5, 5) ➞ 0
+*/
+
 const remainder = (a, b) => {
     return a % b;
 }
