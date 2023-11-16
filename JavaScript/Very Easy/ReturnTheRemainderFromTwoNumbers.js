24e5acb3fba9be4e8537ec0d1cb31ba09e2fe32b
alexsouza10 <alexsouza160298@gmail.com>
2023-11-16 06:46:38 -0300

    feat: ReturnTheRemainderFromTwoNumbers

---
+++ JavaScript/Very Easy/ReturnTheRemainderFromTwoNumbers.js
@@ -0,0 +1,8 @@
+const remainder = (a, b) => {
+    return a % b;
+}
+
+console.log(remainder(1, 3)) //Output: 1
+console.log(remainder(3, 4)) //Output: 3
+console.log(remainder(-9, 45)) //Output: -9
+console.log(remainder(5, 5)) //Output: 0
