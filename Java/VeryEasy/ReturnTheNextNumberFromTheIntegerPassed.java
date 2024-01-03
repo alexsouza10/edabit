b1318c1bb971e1c85e062207311288fb58b265c0
alexsouza10 <alexsouza160298@gmail.com>
2024-01-03 14:32:31 -0300

    feat: returnTheNextNumber

---
+++ Java/VeryEasy/ReturnTheNextNumberFromTheIntegerPassed.java
@@ -0,0 +1,6 @@
+public class ReturnTheNextNumberFromTheIntegerPassed {
+    public static void main(String[] args) {
+        int num = -3, fixed = +1;
+        System.out.println(num + fixed);
+    }
+}
