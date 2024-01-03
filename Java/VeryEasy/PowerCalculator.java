24b4cc2da88c1421179535e43826d91e11cfff85
alexsouza10 <alexsouza160298@gmail.com>
2024-01-03 14:34:16 -0300

    feat: powerCalculator

---
+++ Java/VeryEasy/PowerCalculator.java
@@ -0,0 +1,7 @@
+public class PowerCalculator {
+    public static void main(String[] args) {
+        int voltage = 230, current = 10;
+        int power = voltage * current;
+        System.out.println(power);
+    }
+}
