bc53eaf74f247033d59b0a2d2c3fa99e1961a378
alexsouza10 <alexsouza160298@gmail.com>
2024-01-03 14:33:18 -0300

    feat: upVotes

---
+++ Java/VeryEasy/UpvotesVsDownvotes.java
@@ -0,0 +1,7 @@
+public class UpvotesVsDownvotes {
+    public static void main(String[] args) {
+        int upVotes = 2, downVotes = 33;
+        int result = upVotes - downVotes;
+        System.out.println(result);
+    }
+}
