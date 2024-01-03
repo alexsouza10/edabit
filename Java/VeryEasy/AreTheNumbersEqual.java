196838e4a80422b210e97d0f1f8feb4168c79f3d
alexsouza10 <alexsouza160298@gmail.com>
2024-01-03 14:32:56 -0300

    feat: areTheNumbers

---
+++ Java/VeryEasy/AreTheNumbersEqual.java
@@ -0,0 +1,7 @@
+public class AreTheNumbersEqual {
+    public static void main(String[] args) {
+        int x = 2, y = 22; 
+        boolean areEqual = (x == y) ? true : false;
+            System.out.println("Are the numbers equal? " + areEqual);
+        }
+    }
