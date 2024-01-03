cfcabbfcaae486ab1f5f4183fe9ecebd32951137
alexsouza10 <alexsouza160298@gmail.com>
2024-01-03 14:35:11 -0300

    feat: areaOfTriangle

---
+++ Java/VeryEasy/AreaOfATriangle.java
@@ -0,0 +1,7 @@
+public class AreaOfATriangle {
+    public static void main(String[] args) {
+        int base = 3, height = 2;
+        int triArea = (base * height) / 2;
+        System.out.println(triArea);
+    }    
+}
