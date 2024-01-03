96b361e452719cbdf73ea9c4ecd68277a94ba554
alexsouza10 <alexsouza160298@gmail.com>
2024-01-03 14:33:55 -0300

    feat: findThePerimeter

---
+++ Java/VeryEasy/FindThePerimeterOfARectangle.java
@@ -0,0 +1,7 @@
+public class FindThePerimeterOfARectangle {
+    public static void main(String[] args) {
+        int width = 6, length = 7;
+        int sumFindPerimeter = 2 * (width + length);
+        System.out.println(sumFindPerimeter);
+    }
+}
