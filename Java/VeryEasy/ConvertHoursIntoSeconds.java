6362c68c89aecef4ab99c5ba59a5ed901cce8c48
alexsouza10 <alexsouza160298@gmail.com>
2024-01-03 14:33:33 -0300

    feat: convertHours

---
+++ Java/VeryEasy/ConvertHoursIntoSeconds.java
@@ -0,0 +1,7 @@
+public class ConvertHoursIntoSeconds {
+ public static void main(String[] args) {
+    int hour = 10, seconds = 60 * 60;
+    int conversion = hour * seconds;
+    System.out.println(conversion);
+ }   
+}
