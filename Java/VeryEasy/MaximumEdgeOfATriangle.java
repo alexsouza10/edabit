3936f5f8180c37241952a58b32a0ddc3475f2139
alexsouza10 <alexsouza160298@gmail.com>
2024-01-03 14:34:44 -0300

    feat: maximumEdge

---
+++ Java/VeryEasy/MaximumEdgeOfATriangle.java
@@ -0,0 +1,7 @@
+public class MaximumEdgeOfATriangle {
+    public static void main(String[] args) {
+        int side1 = 8, side2 = 10;
+        int nextEdge = (side1 + side2) -1;
+        System.out.println(nextEdge);
+    }
+}
