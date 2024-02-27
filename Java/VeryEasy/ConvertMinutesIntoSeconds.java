821f8772f5c31afa591d50f78e032e8ead7929c3
alexsouza10 <alexsouza160298@gmail.com>
2024-02-27 16:56:04 -0300

    very_easy-https://edabit.com/challenge/2t8JDxF7wLrg7yJ5E: Convert Minutes into Seconds

---
+++ Java/VeryEasy/ConvertMinutesIntoSeconds.java
@@ -0,0 +1,16 @@
+/*
+Write a function that takes an integer minutes and converts it to seconds.
+
+Examples
+
+convert(5) ➞ 300
+ */
+
+public class ConvertMinutesIntoSeconds {
+    public static void main(String[] args) {
+        int num = 5;
+        int minOfConvesion = 60;
+        int convert = num*minOfConvesion;
+        System.out.println(convert);
+    }
+}
