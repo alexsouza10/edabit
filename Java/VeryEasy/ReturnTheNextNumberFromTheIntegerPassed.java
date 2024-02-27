ac375343a668b6b5eb0b919774b89a0d37227dec
alexsouza10 <alexsouza160298@gmail.com>
2024-02-27 17:03:29 -0300

    very_easy-https://edabit.com/challenge/Qir2pQ2Qp3uwrfJNZ: Return the Next Number from the Integer Passed

---
+++ Java/VeryEasy/ReturnTheNextNumberFromTheIntegerPassed.java
@@ -0,0 +1,15 @@
+/*
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+Examples
+
+addition(9) ➞ 10
+ */
+
+public class ReturnTheNextNumberFromTheIntegerPassed {
+    public static void main(String[] args) {
+        int num = 9;
+        int addition = num + 1;
+        System.out.println(addition);
+    }
+}
