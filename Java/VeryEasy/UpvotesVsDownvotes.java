30a48153cb144fab36319940d486de16bf2ca323
alexsouza10 <alexsouza160298@gmail.com>
2024-02-29 15:59:46 -0300

    very_easy-https://edabit.com/challenge/XJxu5LCnqN9K8SXm5: Get_Vote_Count_Update

--- Java/VeryEasy/UpvotesVsDownvotes.java
+++ Java/VeryEasy/UpvotesVsDownvotes.java
@@ -6,17 +6,27 @@ Given an object containing counts of both upvotes and downvotes, return what vot
 Examples
 
 getVoteCount({ upvotes: 13, downvotes: 0 }) ➞ 13
+getVoteCount({ upvotes: 2, downvotes: 33 }) ➞ -31
+getVoteCount({ upvotes: 132, downvotes: 132 }) ➞ 0
  */
 
+import java.util.Map;
 
 public class UpvotesVsDownvotes {
     public static void main(String[] args) {
-        int getVoteConunt = logicvotes(13, 0);
-        System.out.println(getVoteConunt); //Output: 13
+
+        Map<String, Integer> votes1 = Map.of("upvotes", 13, "downvotes", 0);
+        Map<String, Integer> votes2 = Map.of("upvotes", 2, "downvotes", 33);
+        Map<String, Integer> votes3 = Map.of("upvotes", 132, "downvotes", 132);
+
+        System.out.println("Vote Count 1: " + getVoteCount(votes1)); // Output: Vote Count 1: 13
+        System.out.println("Vote Count 2: " + getVoteCount(votes2)); // Output: Vote Count 2: -31
+        System.out.println("Vote Count 3: " + getVoteCount(votes3)); // Output: Vote Count 3: 0
     }
 
-    static int logicvotes(int upvotes, int downvotes) {
-        int sumvotes = upvotes - downvotes;
-        return sumvotes;
+    public static int getVoteCount(Map<String, Integer> votes) {
+        int upvotes = votes.get("upvotes");
+        int downvotes = votes.get("downvotes");
+        return upvotes - downvotes;
     }
 }
