c218ab987ab267718e8409573b0a79c690d5ddd7
alexsouza10 <alexsouza160298@gmail.com>
2024-03-01 15:21:45 -0300

    very_easy-https://edabit.com/challenge/SquoES5RxWr4CyX6G : Get_First_Value_Update

---
+++ Java/VeryEasy/FlipTheIntBoolean.java
@@ -0,0 +1,19 @@
+public class FlipTheIntBoolean {
+    public static void main(String[] args) {
+        int flipIntBool = reverseNum(1);
+        System.out.println(flipIntBool);
+
+    }
+
+    static boolean reverseNum(int num) {
+        int false = 1;
+        int true = 0;
+        
+        if(num == false) {
+            return true;
+        }else {
+            return false;
+        }
+        return num;
+}
+}
--- Java/VeryEasy/ReturnTheFirstElementOfAnArray.java
+++ Java/VeryEasy/ReturnTheFirstElementOfAnArray.java
@@ -6,20 +6,32 @@
 
 // getFirstValue([1, 2, 3]) ➞ 1
 
+// getFirstValue([80, 5, 100]) ➞ 80
+
+// getFirstValue([-500, 0, 50]) ➞ -500
+
 public class ReturnTheFirstElementOfAnArray {
     public static void main(String[] args) {
-        int[] array = { 1, 2, 3 };
-        int firstElement = getFirstValue(array);
-        System.out.println(firstElement); // Output: 1
-        validateArrays(array); // Output: Test Passed!
+        int[] array1 = { 1, 2, 3 };
+        int[] array2 = { 80, 5, 100 };
+        int[] array3 = { -500, 0, 50 };
+
+        testCase(array1, 1);
+        testCase(array2, 80);
+        testCase(array3, -500);
     }
 
     static int getFirstValue(int[] array) {
         return array[0];
     }
 
-    static void validateArrays(int[] array) {
-        String test = (array.length > 0) ? "Test Passed!" : "Test Reproved";
-        System.out.println(test);
+    static void testCase(int[] array, int expected) {
+        int result = getFirstValue(array);
+        if (result == expected) {
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed!" + java.util.Arrays.toString(array) + ". Expected: " + expected
+                 + ", Not: " + result);
+        }
     }
 }
