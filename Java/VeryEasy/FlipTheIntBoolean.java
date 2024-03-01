96e5888a74cfe2b3c4475858e56b86eafc97ffc5
alexsouza10 <alexsouza160298@gmail.com>
2024-03-01 16:05:26 -0300

    very_easy-https://edabit.com/challenge/EnkJmMjFaivQ5txdW : Flip_Int_Boolean

--- Java/VeryEasy/FlipTheIntBoolean.java
+++ Java/VeryEasy/FlipTheIntBoolean.java
@@ -1,19 +1,31 @@
+// Flip the int Boolean
+
+// Make a function that returns an int boolean opposite of an int boolean given (flip the int boolean).
+
+// Examples:
+
+// flipIntBool(1) ➞ 0
+
+// flipIntBool(0) ➞ 1
+
 public class FlipTheIntBoolean {
     public static void main(String[] args) {
-        int flipIntBool = reverseNum(1);
-        System.out.println(flipIntBool);
+        flipIntBool(0); 
+        flipIntBool(1); 
 
+        System.out.println(flipIntBool(0)); // Output: 1
+        System.out.println(flipIntBool(1)); // Output: 0
+
+        testCases();
     }
 
-    static boolean reverseNum(int num) {
-        int false = 1;
-        int true = 0;
-        
-        if(num == false) {
-            return true;
-        }else {
-            return false;
-        }
-        return num;
-}
+    static int flipIntBool(int x) {
+        int intBool = (x == 0) ? 1 : 0;
+        return intBool;
+    }
+
+    static void testCases() {
+        String test = (flipIntBool(0) == 1 && flipIntBool(1) == 0) ? "All tests passed" : "Not all tests passed";
+        System.out.println(test);
+    }
 }
