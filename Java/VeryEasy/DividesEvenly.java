fb005dd44118b00adfdb4fe20ec80ffbda194d25
alexsouza10 <alexsouza160298@gmail.com>
2024-03-03 15:17:39 -0300

    very_easy-https://edabit.com/challenge/dLXXzJ5hkLAzkyDCm : Divides_Evenly

---
+++ Java/VeryEasy/DividesEvenly.java
@@ -0,0 +1,24 @@
+// Divides Evenly
+
+// Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
+
+// Examples
+// dividesEvenly(98, 7) ➞ true
+// // 98/7 = 14
+
+// dividesEvenly(85, 4) ➞ false
+// // 85/4 = 21.25
+
+public class DividesEvenly {
+    public static void main(String[] args) {
+        checkNum(98, 7);
+        checkNum(85, 4);
+        
+    }
+
+    static boolean checkNum(int a, int b) {
+        boolean checador = (a % b) == 0 ? true : false;
+        System.out.println(checador);
+        return checador;
+    }
+}
