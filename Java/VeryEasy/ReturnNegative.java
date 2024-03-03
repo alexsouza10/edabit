245cd6e21ffec94ad2cd9c66f3a6b9495b396a5b
alexsouza10 <alexsouza160298@gmail.com>
2024-03-03 17:15:47 -0300

    cd /media/home/HD500GB/Projetos/Edabit/Java/VeryEasy ; /usr/bin/env /usr/lib/jvm/java-11-openjdk-amd64/bin/java -cp /home/home/.config/Code/User/workspaceStorage/36c62f1cdb945d3565388d32cd08b416/redhat.java/jdt_ws/VeryEasy_a70a740f/bin ReturnNegative

---
+++ Java/VeryEasy/ReturnNegative.java
@@ -0,0 +1,35 @@
+// Return Negative
+
+// Create a function that takes a number as an argument and returns negative of that number. Return negative numbers without any change.
+
+// Examples
+
+// returnNegative(4) ➞ -4
+
+// returnNegative(5) ➞ -5
+
+// returnNegative(-54) ➞ -54
+
+// returnNegative(0) ➞ 0
+
+public class ReturnNegative {
+    public static void main(String[] args) {
+        returnNegative(4); // Output: -4
+        returnNegative(5); // Output: -5
+        returnNegative(-54); // Output: -54
+        returnNegative(0); // Output: 0
+    }
+
+    static int returnNegative(int num) {
+        if (num > 0) {
+            System.out.println(num - 2 * num);
+            return num;
+        // } else if (num == 0) {
+        //     System.out.println(0);
+        //     return num;
+        } else {
+            System.out.println(num);
+            return (num);
+        }
+    }
+}
